66c94ee3a4d5d86f9c70441321cce783f5ba02b7
amorchep <shpecht.roman@gmail.com>
2020-05-01 22:30:58 +0300

    nextSaveOne

--- Test.py
+++ Test.py
@@ -1,5 +1,24 @@
-x = 9 + 6
-y = 9 +5
-print ("Hello world")
-print ("User new")
+# x = 9 + 6
+# y = 9 + 5
+# a = x - y
+# sms = 'Connecting Users'
+# Location = "Usa"
+# print ("New Users", x)
+# print ('Location', Location)
+# print (sms, a)
+# print (x, y)
 
+
+f = open('new.txt', 'w+')
+f.write('_____________________''\n'' ------- >  Bangs 6''\n''_____________________')
+f.close()
+try:
+    f = open('new.txt')
+    for line in f:
+        print(line)
+except IOError:
+    print("An IOError has occurred!")
+finally:
+# data = f.readlines() # Вывод в одну линию
+# print(data)
+    f.close()
