13d0fe30bcff78d96d1453034de523677441bab6
amorchep <shpecht.roman@gmail.com>
2020-05-03 19:57:11 +0300

    Scaner

---
+++ Scaner.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+mas = [20, 21, 22, 23, 25, 42, 43, 53, 67, 69, 80, 110, 115, 123, 137, 138, 139, 143, 161, 179, 443, 445, 514, 515, 993]
+print('Start')
+host = input('Pls  enter text IP or name Domain: ')
+print ('------------------------------')
+print ('Scaning ports')
+print ('------------------------------')
+for port in mas:
+    s = socket.socket()
+    s.settimeout(1)
+    try:
+        s.connect((host, port))
+    except socket.error:
+        pass
+    else:
+        s.close
+        print(host + ':' + str(port) + ' port active')
+print ('------------------------------')
+print ('END')
